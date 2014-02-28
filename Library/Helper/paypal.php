a284105ec6697abcd6c7ce9a835aba3287a34fd5
Thibault <thibault.dulon@gmail.com>
2014-03-01 00:01:20 +0100

    paypal class helper debut

--- Library/Helper/paypal.php
+++ Library/Helper/paypal.php
@@ -6,6 +6,44 @@
       Déscription : Permet de gérer le paiement via paypal
      */
     class Paypal extends Helper {
+	private $_username = "sell_api1.localhost.fr";
+	private $_password = "1393605614";
+	private $_signature = "ATM9fpmKSuPGPsQ.TNNoHOvNfnzMAIHsSTo8Ioj7.fhhmklFDRL83E77";
+	
+	private $_params = array(
+	    'METHOD' => 'SetExpressCheckout',
+	    'VERSION' => '109',
+
+	    'PAYMENTREQUEST_0_CURRENCYCODE' => 'EUR'
+	);
 	
 	
+	
+	public function setUsername($arg){
+	    $this->_username = $arg;
+	    $this->_params['USER'] = $arg;
+	}
+	public function setPassword($arg){
+	    $this->_password = $arg;
+	    $this->_params['SIGNATURE'] = $arg;
+	}
+	public function setSignature($arg){
+	    $this->_signature = $arg;
+	    $this->_params['PWD'] = $arg;
+	}
+	public function setReturnUrl($arg){
+	    $this->_params['RETURNURL'] = $arg;
+	}
+	public function setCancelUrl($arg){
+	    $this->_params['CANCELURL'] = $arg;
+	}
+	public function setPort($arg){
+	    $this->_params['PAYMENTREQUEST_0_SHIPPINGAMT'] = $arg;
+	}
+	public function setTotal($arg){
+	    $this->_params['PAYMENTREQUEST_0_ITEMAMT'] = $arg;
+	}
+	public function setTotalTTC(){
+	    $this->_params['PAYMENTREQUEST_0_ITEMAMT'] = $this->_params['PAYMENTREQUEST_0_SHIPPINGAMT'] + $this->_params['PAYMENTREQUEST_0_ITEMAMT'];
+	}
     }
--- Website/MVC/View/Home/testPaypal.php
+++ Website/MVC/View/Home/testPaypal.php
@@ -1,26 +1,83 @@
+<link type="text/css" rel="stylesheet" href="<?php echo __css_directory__;?>/home.css" />
+<title>FLFramework - Le framework PHP français</title>
+
 <?php
+    // inclure ci-dessus les balises à inclure dans la balise <head> du layout
+    $head = $this->RegisterViewHead();
+    // START CONTENT
 
-$products = array(
-    array(
-	"name" => "produit1",
-	"price" => 10.0,
-	"priceTVA" => 12.0,
-	"quantity" => 1
-    ),
-    array(
-	"name" => "produit2",
-	"price" => 25.5,
-	"priceTVA" => 30.50,
-	"quantity" => 2
-    )
-);
+    $products = array(
+	array(
+	    "name" => "produit1",
+	    "price" => 10.0,
+	    "priceTVA" => 12.0,
+	    "quantity" => 1
+	),
+	array(
+	    "name" => "produit2",
+	    "price" => 25.5,
+	    "priceTVA" => 30.50,
+	    "quantity" => 2
+	)
+    );
+    
+    $Paypal = new \fitzlucassen\FLFramework\Library\Helper\Paypal();
+    $Paypal->setReturnUrl('http://flframework:81/' . fitzlucassen\FLFramework\Library\Core\Router::GetUrl("home", "testpaypal"));
+    $Paypal->setCancelUrl('http://flframework:81/' . fitzlucassen\FLFramework\Library\Core\Router::GetUrl("home", "testpaypal"));
+    $Paypal->setPort(10);
+    $Paypal->setTotal(73);
+    $Paypal->setTotalTTC();
+    
+    foreach ($products as $k => $pro){
+	$params['L_PAYMENTREQUEST_0_NAME' . $k] = $pro['name'];
+	$params['L_PAYMENTREQUEST_0_DESC' . $k] = '';
+	$params['L_PAYMENTREQUEST_0_AMT' . $k] = $pro['priceTVA'];
+	$params['L_PAYMENTREQUEST_0_QTY' . $k] = $pro['quantity'];
+    }
+    
+    $params = http_build_query($params);
+    $endpoint = 'https://api-3T.sandbox.paypal.com/nvp';
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+	CURLOPT_URL => $endpoint,
+	CURLOPT_POST => 1,
+	CURLOPT_POSTFIELDS => $params,
+	CURLOPT_RETURNTRANSFER => 1,
+	CURLOPT_SSL_VERIFYPEER => false,
+	CURLOPT_SSL_VERIFYHOST => false,
+	CURLOPT_VERBOSE => 1
+    ));
+    $responseArray = array();
+    parse_str(curl_exec($curl), $responseArray);
 
-$total = 61.0;
-$totalttc = 73.0;
-$paypal = "#";
-
+    if(curl_errno($curl)){
+	var_dump(curl_error($curl));
+	curl_close($curl);
+	die();
+    }
+    else {
+	if($responseArray['ACK'] == 'Success'){
+	    curl_close($curl);
+	}
+	else {
+	    var_dump($responseArray);
+	    curl_close($curl);
+	    die();
+	}
+    }
+    $paypal = 'https://www.sandbox.paypal.com/webscr?cmd=_express-checkout&useraction=commit&token=' . $responseArray['TOKEN'];
+    
 ?>
 
 <div id="cart">
-    
+    <?php
+	foreach($products as $p){
+	    echo "<h2>" . $p['name'] . "</h2>";
+	    echo "<p>" . $p['price'] . "</p>";
+	    echo "<p>" . $p['priceTVA'] . "</p>";
+	    echo "<p>" . $p['quantity'] . "</p>";
+	}
+	echo '<h3>' . $totalttc . '</h3>';
+    ?>
+    <a href="<?php echo $paypal; ?>">payer</a>
 </div>
