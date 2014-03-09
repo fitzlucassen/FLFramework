3ea53e73596d3a7ab344ab9361f58f560b15f641
Thibault <thibault.dulon@gmail.com>
2014-03-09 14:47:49 +0100

    auth helper improvement

--- Library/Helper/auth.php
+++ Library/Helper/auth.php
@@ -7,41 +7,93 @@
       Déscription : Permet de gérer la couche autentification. (Connexion / Deconnexion d'un utilisateur)
      */
     class Auth extends Helper{
-	private static $_user = null;
-	private static $_repositoryManager = null;
-	
-	/**
-	 * connect --> connecte un utilisateur en session s'il existe en bdd retourne faux sinon
-	 * @param object $manager
-	 * @return boolean/User
-	 */
-	public static function connect($manager) {
-	    $this->_repositoryManager = $manager;
-	    
-	    $UserRepository = $this->_repositoryManager->get('User');
-	    $user = $UserRepository->getByLogin($login, $pwd);
-	    
-	    if(array_key_exists($user, 'id')){
-		Session::Write("Auth", $user['id']);
-		self::$_user = $user;
-		return self::$_user;
-	    }
-	    else{
-		return false;
-	    }
-	}
+    	private $_siteKey = "fitzlucassen\FLFramework";
+		private $_user = null;
+		private $_repositoryManager = null;
+		private $_session = null;
+		private $_params = array(
+			'table' => 'user',
+			'primaryKeyField' => 'id',
+			'loginField' => 'login',
+			'passwordField' => 'password',
+			'adminField' => 'isAdmin',
+			'encryptedPassword' => true
+		);
+		
+		/**
+		 * Constructeur
+		 * @param $manager
+		 * @param $params
+		 */
+		public function __construct($manager, $params = null){
+			$this->_repositoryManager = $manager;
+			$this->_session = new Session();
 
-	/**
-	 * disconnect --> déconnecte un utilisateur de la session courante
-	 */
-	public static function disconnect() {
-	    Session::clear("Auth");
-	}
-	
-	/***********
-	 * GETTERS *
-	 ***********/
-	public static function getUser() {
-	    return self::$_user;
-	}
+			if(isset($params) && is_array($params)){
+				$this->_params = array_merge($this->_params, $params);
+			}
+		}
+		/**
+		 * connect --> connecte un utilisateur en session s'il existe en bdd retourne faux sinon
+		 * @param object $manager
+		 * @return boolean/User
+		 */
+		public static function connect($login, $pwd) {
+		    $UserRepository = $this->_repositoryManager->get(ucwords($this->_params['table']));
+
+		    $user = $UserRepository->getBy($this->_params['loginField'], $login);
+		    $attrPwd = 'get' . ucwords($this->_params['passwordField']) . '()';
+		    
+		    $attr = 'get' . ucwords($this->_params['primaryKeyField']) . '()';
+
+		    if(isset($user)){
+		    	if(is_array($user))
+		    		$user = $user[0];
+		    	
+			    $id = $user->$attr;
+			    if(isset($id) && !empty($id) && $id > 0 && $user->$attrPwd == $this->hashData($pwd)){
+					$this->_session->Write("Auth", $id);
+					$this->_user = $user;
+					return $this->_user;
+			    }
+			    else{
+					return false;
+			    }
+		    }
+		    else {
+		    	return false;
+		    }
+		}
+
+		/**
+		 * disconnect --> déconnecte un utilisateur de la session courante
+		 */
+		public function disconnect() {
+		    $this->_session->Clear("Auth");
+		    $this->_user = null;
+		}
+
+		/**
+		 * isAdmin -> retourne vrai si le user est administrateur
+		 * @return boolean
+		 */
+		public function isAdmin(){
+			$attr = 'get' . ucwords($this->_params['adminField']) . '()';
+			if(isset($this->_user) && $this->_user->$attr){
+				return true;
+			}
+			else
+				return false;
+		}
+
+		private function hashData($data){
+			return md5($data);
+		}
+		
+		/***********
+		 * GETTERS *
+		 ***********/
+		public function getUser() {
+		    return $this->_user;
+		}
     }
